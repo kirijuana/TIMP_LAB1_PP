3324d94785fccd3a755be69bf17e85879e3b245f
kirijuana <sazkirand67@gmail.com>
2019-03-04 16:56:46 +0700

    ADD OUTRECT

---
+++ TIMP_LAB1/container_OutRect.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include <fstream>
+#include "container_atd.h"
+#include "shape_atd.h"
+using namespace std;
+namespace simple_shapes {
+	// Сигнатуры требуемых внешних функций
+	void Out(shape &s, ofstream &ofst);
+	// Вывод содержимого контейнера в указанный поток
+	void OutRect(container &c, ofstream &ofst) {
+		ofst << "Only rectangles." << endl;
+		for (int i = 0; i < c.len; i++) {
+			ofst << i << ": ";
+			if (c.cont[i]->k == shape::RECTANGLE)
+				Out(*(c.cont[i]), ofst);
+			else
+				ofst << endl;
+		}
+	}
+} // end simple_shapes namespace
